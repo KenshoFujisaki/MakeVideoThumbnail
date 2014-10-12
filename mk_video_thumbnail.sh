ed17cb11e09e435cc235fee50c006bdfe3d6633a
Kensho Fujisaki <kensho.fujisaki@gmail.com>
2014-10-12 19:21:49 +0900

    Update mk_video_thumbnail.sh
    fpsが存在しない場合のエラーを追加
    durationで先頭が0(09など)の場合に，適切に計算出来ないバグを修正

--- mk_video_thumbnail.sh
+++ mk_video_thumbnail.sh
@@ -7,12 +7,14 @@ tiles_v=8          # サムネイルタイルの垂直方向数
 log_file_path="./mk_thumbnail_`date '+%Y%m%d%H%M%S'`.log" # ログファイルパス
 
 # サムネイル作成対象のファイル数の取得
+IFS=$'\n';
 input_files_cmd='$find $input_dir_path -type f -maxdepth 1 \( -name "*.flv" -o -name "*.mp4" -o -name "*.avi" \)'
 nof_files=`eval "$input_files_cmd" | wc -l`
 echo "#input files: $nof_files" | tee $log_file_path
 
 counter=1
 for file in `eval "$input_files_cmd"`; do
+  IFS=$' \t\n'
     
   printf "processing:$file ($counter/$nof_files) is started... " | tee -a $log_file_path
   counter=$(($counter + 1))
@@ -24,22 +26,26 @@ for file in `eval "$input_files_cmd"`; do
   fi
   
   # 動画フレーム数の計算（Frames = Duration * FPS）
-  hms=(`echo \`$ffmpeg -i "$file" 2>&1 | awk '/Duration/{print $2}' | sed 's/\..*//g' | tr -s ':' ' '\``)
+  hms=(`echo \`$ffmpeg -i "$file" 2>&1 | awk '/Duration/{print $2}' | sed 's/\..*//g' | tr -s ':' ' ' \` `)
   if [ ${#hms[*]} -eq 0 ]; then
-    printf "error (duration is not found).\n" | tee -a $log_file_path
+    printf "error (duration is not defined).\n" | tee -a $log_file_path
     continue
   fi
-  duration_sec=$((${hms[0]} * 3600 + ${hms[1]} * 60 + ${hms[2]}))
+  duration_sec=`expr ${hms[0]} \* 3600 + ${hms[1]} \* 60 + ${hms[2]} \* 1`
   src_fps=`$ffmpeg -i "$file" 2>&1 | awk '/fps/{print $0}' | sed 's/\ fps.*//' | sed 's/.*\ //'`
+  if [ "$src_fps" = "" ]; then
+    printf "error (fps is not defined).\n" | tee -a $log_file_path
+    continue
+  fi
   frames=`echo "$duration_sec * $src_fps" | bc | sed 's/\..*//g'`
 
   # サムネイル作成処理
   tiles=$(($tiles_h * $tiles_v))
-  thumb_fps=$(($frames / $tiles)) #3
-  if [ $thumb_fps -gt 1000 ]; then #2
+  thumb_fps=`expr $frames \/ $tiles` 
+  if [ $thumb_fps -gt 1000 ]; then 
     thumb_fps=1000
   fi
-  $ffmpeg -y -i "$file" -vf thumbnail=${thumb_fps},tile=${tiles_h}x${tiles_v},scale=1920:-1 "${file%.*}_thumb.jpg" >/dev/null 2>&1 
+  $ffmpeg -y -i "$file" -vf thumbnail=${thumb_fps},tile=${tiles_h}x${tiles_v},scale=1920:-1 "${file%.*}_thumb.jpg" >/dev/null 2>&1 | tr -d '\n' | tee -a $log_file_path
 
   printf "done.\n" | tee -a $log_file_path
 done
